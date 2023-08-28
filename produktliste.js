47fc4549a21c332835929431122c7196cdfa6e38
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-28 13:57:10 +0200

    appende

--- produktliste.js
+++ produktliste.js
@@ -9,5 +9,22 @@ function showProducts(products) {
 function showProduct(product) {
   console.log(product);
   const template = document.querySelector("#ProductTemplate").content;
-  const copy = template.cloneNote(true);
+  const copy = template.cloneNode(true);
+
+  document.querySelector("main").appendChild(copy);
 }
+
+/*      <template id="ProductTemplate">
+            <article class="Product">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/1573.webp"
+                    alt="Sahara Team India Fanwear Round Neck Jersey">
+                <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
+                <p class="subtle">Tshirts | Nike</p>
+                <p class="price">DKK 1595.-</p>
+                <a href="produkt.html">Read more</a>
+            </article>
+        </template>  */
+
+/* Object { id: 1526, gender: "Unisex", category: "Accessories", subcategory: "Bags", 
+articletype: "Backpacks", season: "Fall", productionyear: 2010, usagetype: "Sports", 
+productdisplayname: "Big Cat Backpack Black", price: 1299, … } */
