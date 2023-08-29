1dbb14515ac87230fd12ee1d3a72dc54a11477da
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-29 20:57:52 +0200

    price working

--- produkt.js
+++ produkt.js
@@ -7,5 +7,22 @@ fetch("https://kea-alt-del.dk/t7/api/products/" + id)
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  const productImg = document.querySelector(".product_img img");
+  const productText = document.querySelector(".product_text");
+
+  productImg.src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+
+  const priceText = document.createElement("h3");
+  priceText.textContent = `Price: DKK ${product.price}`;
+  productText.appendChild(priceText);
+
+  const sizeText = document.createElement("h4");
+  sizeText.textContent = `Size: ${product.size}`;
+  productText.appendChild(sizeText);
+
+  const stockText = document.createElement("h4");
+  stockText.textContent = `In Stock: ${product.stock}`;
+  productText.appendChild(stockText);
+
+  // Add more lines here to populate other information like category, brand, etc.
 }
